b86dc1dde3de78402ea000c3265c8d02795a0a6b
pavemen <jason@bryngelson.net>
2013-06-09 12:20:20 -0700

    removed additonal credits

--- boards/mingle/forums.php
+++ boards/mingle/forums.php
@@ -7,7 +7,6 @@
  * License: http://www.mybb.com/about/license
  *
  * $Id$
- * Modified for Mingle Forums 1.0 by http://www.communityplugins.com
  */
 
 // Disallow direct access to this file for security reasons
@@ -27,22 +26,22 @@ class MINGLE_Converter_Module_Forums extends Converter_Module_Forums {
 	function import()
 	{
 		global $import_session, $db;
-		
+
 		$query = $this->old_db->simple_select("forum_forums", "*", "1=1", array('order_by' => 'id', 'order_dir' => 'ASC', 'limit_start' => $this->trackers['start_forums'], 'limit' => $import_session['forums_per_screen']));
 		while($forum = $this->old_db->fetch_array($query))
 		{
 			$forum['import_pid'] = $forum['parent_id'];
 			$fid = $this->insert($forum);
-			
+
 			// TODO: forum subscriptions? use wp_options.option_name = mf_forum_subscriptions_{fid} and lookup users based on list of emails
 
 		}
 	}
-	
+
 	function convert_data($data)
 	{
 		$insert_data = array();
-		
+
 		// Mingle Values
 		$insert_data['import_fid'] = intval($data['id']);
 		$insert_data['import_pid'] = intval($data['parent_id']);
@@ -51,17 +50,17 @@ class MINGLE_Converter_Module_Forums extends Converter_Module_Forums {
 		$insert_data['disporder'] = $data['sort'];
 
 		$insert_data['type'] = 'f';
-		
-	
+
+
 		// TODO: last post data?
-		
+
 		return $insert_data;
 	}
-	
+
 	function fetch_total()
 	{
 		global $import_session;
-		
+
 		// Get number of forums
 		if(!isset($import_session['total_forums']))
 		{
@@ -69,10 +68,10 @@ class MINGLE_Converter_Module_Forums extends Converter_Module_Forums {
 			$import_session['total_forums'] = $this->old_db->fetch_field($query, 'count');
 			$this->old_db->free_result($query);
 		}
-		
+
 		return $import_session['total_forums'];
 	}
-	
+
 	/**
 	 * Correctly associate any forums with their correct parent ids. This is automagically run after importing
 	 * forums.
@@ -87,21 +86,21 @@ class MINGLE_Converter_Module_Forums extends Converter_Module_Forums {
 			LEFT JOIN ".TABLE_PREFIX."forums f2 ON (f2.import_fid=f.import_pid)
 			WHERE f.import_pid != 0 AND f.pid = 0
 		");
-	*/	
+	*/
 		$query = $db->query("
 			SELECT f1.fid, f2.fid AS updatefid
 			FROM mybb_forums f1
 			INNER JOIN mybb_forums f2 ON f1.import_pid = f2.import_fid
 			WHERE f2.type = 'c'
 		");
-		
+
 		while($forum = $db->fetch_array($query))
 		{
 			$db->update_query("forums", array('pid' => $forum['updatefid'], 'parentlist' => $forum['updatefid'].",".$forum['fid']), "fid='{$forum['fid']}'", 1);
 		}
-		
+
 		//TODO: fix for subforums and original query does not seem to work at all
-	
+
 	}
 }
 
--- boards/mingle/threads.php
+++ boards/mingle/threads.php
@@ -7,7 +7,6 @@
  * License: http://www.mybb.com/about/license
  *
  * $Id$
- * Modified for Mingle Forums 1.0 by http://www.communityplugins.com
  */
 
 // Disallow direct access to this file for security reasons
@@ -23,7 +22,7 @@ class MINGLE_Converter_Module_Threads extends Converter_Module_Threads {
 		'progress_column' => 'id',
 		'default_per_screen' => 1000,
 	);
-	
+
 	var $get_poll_pid_cache = array();
 
 	function import()
@@ -33,37 +32,37 @@ class MINGLE_Converter_Module_Threads extends Converter_Module_Threads {
 		$query = $this->old_db->simple_select("forum_threads", "*", "1=1", array('order_by' => 'id', 'order_dir' => 'ASC', 'limit_start' => $this->trackers['start_threads'], 'limit' => $import_session['threads_per_screen']));
 		while($thread = $this->old_db->fetch_array($query))
 		{
-			$this->insert($thread);		
+			$this->insert($thread);
 
 			// TODO: thread subscriptions? use wp_options.option_name = mf_thread_subscriptions_{tid} and lookup users based on list of emails
 
 		}
 	}
-	
+
 	function convert_data($data)
 	{
 		$insert_data = array();
-		
+
 		// bbPress values
 		$insert_data['import_tid'] = $data['id'];
-		
+
 		$insert_data['sticky'] = ($data['status'] == 'sticky' ? 1 : 0);
 		$insert_data['closed'] = $data['closed'];
-		
+
 		$insert_data['fid'] = $this->get_import->fid($data['parent_id']);
 		$insert_data['dateline'] = strtotime($data['date']);
 		$insert_data['subject'] = encode_to_utf8($this->bbcode_parser->convert_title($data['subject']), "forum_threads", "threads");
 		$insert_data['uid'] = $this->get_import->uid($data['starter']);
 		$insert_data['import_uid'] = $data['starter'];
 		$insert_data['views'] = $data['views'];
-		
+
 		return $insert_data;
 	}
-	
+
 	function fetch_total()
 	{
 		global $import_session;
-		
+
 		// Get number of threads
 		if(!isset($import_session['total_threads']))
 		{
@@ -71,7 +70,7 @@ class MINGLE_Converter_Module_Threads extends Converter_Module_Threads {
 			$import_session['total_threads'] = $this->old_db->fetch_field($query, 'count');
 			$this->old_db->free_result($query);
 		}
-		
+
 		return $import_session['total_threads'];
 	}
 
