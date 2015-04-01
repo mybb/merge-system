5e89474e460b4d3f03bf75837389feb915105b0a
JN-Jones <Jones_h1@web.de>
2015-04-01 14:11:16 +0200

    WIP #97 Private Message module
    Support for left messages in XenForo

--- boards/xenforo/privatemessages.php
+++ boards/xenforo/privatemessages.php
@@ -26,9 +26,10 @@ class XENFORO_Converter_Module_Privatemessages extends Converter_Module_Privatem
 		global $import_session;
 		
 		$query = $this->old_db->query("
-			SELECT m.*, c.title
+			SELECT m.*, c.title, r.recipient_state
 			FROM ".OLD_TABLE_PREFIX."conversation_message m
 			LEFT JOIN ".OLD_TABLE_PREFIX."conversation_master c ON(c.conversation_id=m.conversation_id)
+			LEFT JOIN ".OLD_TABLE_PREFIX."conversation_recipient r ON(r.conversation_id=m.conversation_id AND r.user_id=m.user_id)
 			LEFT JOIN ".OLD_TABLE_PREFIX."ip i ON(i.ip_id=m.ip_id)
 			LIMIT ".$this->trackers['start_privatemessages'].", ".$import_session['privatemessages_per_screen']
 		);			
@@ -55,30 +56,50 @@ class XENFORO_Converter_Module_Privatemessages extends Converter_Module_Privatem
 		while($rec = $this->old_db->fetch_array($rec_query))
 		{
 			$rec['user_id'] = $this->get_import->uid($rec['user_id']);
-			$to_send[] = $rec;
+
+			// "deleted_ignored" means he left the conversation and doesn't want to receive new messages
+			if($rec['recipient_status'] != 'deleted_ignored')
+			{
+				$to_send[] = $rec;
+			}
+
 			$recipients['to'][] = $rec['user_id'];
 		}
 
 		$insert_data['recipients'] = serialize($recipients);
 
 		// Now save a copy for every user involved in this pm
-		// First one for the sender
-		$insert_data['uid'] = $insert_data['fromid'];
-		if(count($recipients['to']) == 1)
+		// First one for the sender - if he hasn't deleted it
+		if($data['recipient_status'] != 'deleted_ignored')
 		{
-			$insert_data['toid'] = $recipients['to'][0];
-		}
-		else
-		{
-			$insert_data['toid'] = 0; // multiple recipients
-		}
-		$insert_data['status'] = PM_STATUS_READ; // Read - of course
-		$insert_data['readtime'] = 0;
-		$insert_data['folder'] = PM_FOLDER_OUTBOX;
+			$insert_data['uid'] = $insert_data['fromid'];
+			if (count($recipients['to']) == 1)
+			{
+				$insert_data['toid'] = $recipients['to'][0];
+			}
+			else
+			{
+				$insert_data['toid'] = 0; // multiple recipients
+			}
+			$insert_data['status'] = PM_STATUS_READ; // Read - of course
+			$insert_data['readtime'] = 0;
 
-		$data = $this->prepare_insert_array($insert_data);
-		unset($data['import_pmid']);
-		$db->insert_query("privatemessages", $data);
+			if($data['recipient_state'] == 'deleted')
+			{
+				$insert_data['folder'] = PM_FOLDER_TRASH;
+			}
+			else
+			{
+				$insert_data['folder'] = PM_FOLDER_OUTBOX;
+			}
+
+			if(count($to_send) > 0)
+			{
+				$data = $this->prepare_insert_array($insert_data);
+				unset($data['import_pmid']);
+				$db->insert_query("privatemessages", $data);
+			}
+		}
 
 		foreach($to_send as $key => $rec)
 		{
@@ -91,7 +112,15 @@ class XENFORO_Converter_Module_Privatemessages extends Converter_Module_Privatem
 				$insert_data['status'] = PM_STATUS_READ;
 			}
 			$insert_data['readtime'] = $rec['last_read_date'];
-			$insert_data['folder'] = PM_FOLDER_INBOX;
+
+			if($rec['recipient_state'] == 'deleted')
+			{
+				$insert_data['folder'] = PM_FOLDER_TRASH;
+			}
+			else
+			{
+				$insert_data['folder'] = PM_FOLDER_INBOX;
+			}
 
 			// The last pm will be inserted by the main method, so we only insert x-1 here
 			if($key < count($to_send)-1)
